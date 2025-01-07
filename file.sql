bfba4a15cc53dfeb0b45561c381dbb9d9d8b63e9
Artem Derkachevskyi <derkachevskyiartem@gmail.com>
2025-01-07 09:27:59 +0200

    Change domen

--- file.sql
+++ file.sql
@@ -2,12 +2,12 @@ CREATE TABLE workers (
   id SERIAL PRIMARY KEY,
   full_name VARCHAR(255) NOT NULL,
   department VARCHAR(255),
-  birthday DATE CHECK (birthday <= CURRENT_DATE - INTERVAL '18 years' AND birthday >= '1895-01-06'),
+  birthday DATE CHECK (birthday <= current_date - INTERVAL '18 years' AND birthday >= '1895-01-06'),
   salary NUMERIC(10, 2)
 );
 
 ALTER TABLE workers
-ADD COLUMN vacation_days INT DEFAULT 0 CHECK (vacation_days >= 0);
+ADD COLUMN vacation_days SMALLINT DEFAULT 0 CHECK (vacation_days >= 0);
 
 ALTER TABLE workers
 ADD COLUMN email VARCHAR(255) UNIQUE;
